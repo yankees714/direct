0653a5092b492e73fe72a6b874c23607512f55f9
Brian Jacobel <bjacobel@bowdoin.edu>
2013-06-02 16:01:12 -0400

    added some static files

--- search/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from search import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>\d+)/detail/$', views.DetailView.as_view(), name='detail')
-)
