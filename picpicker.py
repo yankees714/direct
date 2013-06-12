1c8c13ff4fb7bdc7d991c02141a309a070cb1354
Brian Jacobel <bjacobel@bowdoin.edu>
2013-06-11 21:31:14 -0400

    sized pictures down to 300px wide, added to assets

--- picpicker.py
+++ picpicker.py
@@ -3,12 +3,20 @@ import Image
 import glob
 import os
 
-pics = glob.glob('*')
+pics = glob.glob('pics/*')
 pics_to_del = list()
 
 for pic in pics:
     try:
         im=Image.open(pic)
     except IOError:
-        os.system("mv "+pic+" ./trash")
-    
+        os.system("rm "+pic)
+    else:
+        size = im.size
+        origWide = size[0]
+        origHeig = size[1]
+        newWide = 300
+        newHeig = int(origHeig*(float(newWide)/float(origWide)))
+        newSize = (newWide, newHeig)
+        smaller = im.resize(newSize, Image.ANTIALIAS)
+        smaller.save("oracleapp/assets/img/"+pic, "JPEG")
