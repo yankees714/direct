dd05513defece743a82379057c02672eb9fbf5f5
Andrew Daniels <andrew.daniels714@gmail.com>
2014-09-05 00:05:02 -0400

    Names and year are working

--- update.py
+++ update.py
@@ -4,58 +4,113 @@
 import re
 import sqlite3
 import os
-import psycopg2
+import string
+
+from robobrowser import RoboBrowser
+# import psycopg2
 
 
 # if os.getcwd() == "/app":
 #     conn_string = "host='ec2-54-225-105-169.compute-1.amazonaws.com' dbname='dct2sfiea871d8' user='nldurbrhtujxzj' password='bqJaaIxKpfVhrYd-svszgiGeLE'"
 #     connection = psycopg2.connect(conn_string)
 # else:
-connection = sqlite3.connect('oracleapp/default.db')
+# connection = sqlite3.connect('oracleapp/default.db')
 
-db = connection.cursor()
+# db = connection.cursor()
 
-db.execute('''DROP TABLE search_person''')
-db.execute('''CREATE TABLE search_person (id int, fname text, mname text, lname text, suffix text, year int, su text, email text, phone text, apt text)''')
-connection.commit()
+# db.execute('''DROP TABLE search_person''')
+# db.execute('''CREATE TABLE search_person (id int, fname text, mname text, lname text, suffix text, year int, su text, email text, phone text, apt text)''')
+# connection.commit()
 
 
-students = []
+browser = RoboBrowser()
+letters = [letter for letter in string.ascii_lowercase]
 id = 1
 
-for student in students:
-    name = ""
+for letter in letters:
+    browser.open('http://www.bowdoin.edu/BowdoinDirectory/lookup.jsp')
 
-    lname = name.split(", ")[0].strip()
-    given = name.split(", ")[1].strip()
+    form = browser.get_form(id='sch')
+    form["ln"].value = letter
+    form["so"].value = "stu"
+    print form
 
-    if len(name.split(", ")) == 3:
-        suffix = name.split(", ")[2]
-    else:
-        suffix = ""
+    browser.submit_form(form)
 
-    fname = given.split(" ")[0].strip()
-    mname = ' '.join(given.split(" ")[1:])
+    students = browser.select('.person')
+    for student in students:
+        header = student.select("h3")
+        info = header[0].text.split("\n")
 
-    year = "20"+''
+        name = info[0].strip()
 
-    su = ""
+        lname = name.split(", ")[0].strip()
+        given = name.split(", ")[1].strip()
 
-    email = ""
+        if len(name.split(", ")) == 3:
+            suffix = name.split(", ")[2]
+        else:
+            suffix = ""
 
-    phone = ""
-    if not re.match("[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]", phone):
-        phone = "Unknown"
+        fname = given.split(" ")[0].strip()
+        mname = ' '.join(given.split(" ")[1:])
 
-    # the dorms are super hard because they're not a constant number of words and occasionally don't exist
-    apt = ""
+        print fname, mname, lname
+        
+        matches = re.findall(r"\'(\d*)", info[1])
+        if matches:
+            year = "20" + matches[0]
+        else:
+            year = "" 
+        print year
+        
 
-    if os.getcwd() == "/app":   #postgres
-        db.execute("INSERT INTO search_person VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (id, fname, mname, lname, suffix, year, su, email, phone, apt))
-    else:   #sqlite
-        db.execute("INSERT INTO search_person VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (id, fname, mname, lname, suffix, year, su, email, phone, apt))
-    connection.commit()
+        details = student.select(".pdetail")
+        # details = details[0].text.replace("\n"," ")
+        # print re.findall(r"Email:   (.*)\@bowdoin\.edu", details)[0]
 
-    id+=1
+        # if os.getcwd() == "/app":   #postgres
+        #     db.execute("INSERT INTO search_person VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (id, fname, mname, lname, suffix, year, su, email, phone, apt))
+        # else:   #sqlite
+        #     db.execute("INSERT INTO search_person VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (id, fname, mname, lname, suffix, year, su, email, phone, apt))
+        # connection.commit()
+
+        id+=1
+
+
+# for student in students:
+#     name = ""
+
+#     lname = name.split(", ")[0].strip()
+#     given = name.split(", ")[1].strip()
+
+#     if len(name.split(", ")) == 3:
+#         suffix = name.split(", ")[2]
+#     else:
+#         suffix = ""
+
+#     fname = given.split(" ")[0].strip()
+#     mname = ' '.join(given.split(" ")[1:])
+
+#     year = "20"+''
+
+#     su = ""
+
+#     email = ""
+
+#     phone = ""
+#     if not re.match("[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]", phone):
+#         phone = "Unknown"
+
+#     # the dorms are super hard because they're not a constant number of words and occasionally don't exist
+#     apt = ""
+
+#     if os.getcwd() == "/app":   #postgres
+#         db.execute("INSERT INTO search_person VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (id, fname, mname, lname, suffix, year, su, email, phone, apt))
+#     else:   #sqlite
+#         db.execute("INSERT INTO search_person VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (id, fname, mname, lname, suffix, year, su, email, phone, apt))
+#     connection.commit()
+
+#     id+=1
 
 db.close()
