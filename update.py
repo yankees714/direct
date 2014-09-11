497be9efab97ddf72e965ab1b3a478755f4a8021
Andrew Daniels <andrew.daniels714@gmail.com>
2014-09-11 10:23:15 -0400

    No dorm might mean off campus

--- update.py
+++ update.py
@@ -38,12 +38,15 @@ letters = [letter for letter in string.ascii_lowercase]
 id = 1
 
 # Login
-browser.open('https://www.bowdoin.edu/BowdoinDirectory/rmSignon.jsp')
-form = browser.get_forms()[1]
-form["uname"] = USERNAME
-form["pword"] = PASSWORD
-print "Logging in...",
-browser.submit_form(form)
+# browser.open('https://www.bowdoin.edu/BowdoinDirectory/rmSignon.jsp')
+# form = browser.get_forms()[1]
+# form["uname"] = USERNAME
+# form["pword"] = PASSWORD
+# print "Logging in...",
+# browser.submit_form(form)
+
+# Only do external access for now
+browser.open("http://www.bowdoin.edu/BowdoinDirectory/lookup.jsp")
 
 # Make sure we actually logged in
 if browser.select("#sch"):
@@ -113,7 +116,7 @@ for letter in letters:
         if not email:
             email = "Unknown"
         if not apt:
-            apt = "Unknown"
+            apt = "Off Campus/Unknown"
         if not re.match("[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]", phone):
             phone = "Unknown"
 
